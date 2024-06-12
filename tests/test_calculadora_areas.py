871f56efdae5450830a73a21a43f40f2ee63c16b
felipe <felipezan23@gmail.com>
2024-06-11 21:10:06 -0300

    adicionei teste

---
+++ tests/test_calculadora_areas.py
@@ -0,0 +1,27 @@
+import unittest
+from calculadora_de_areas import CalculadoraDeAreas
+
+class TestCalculadoraDeAreas(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = CalculadoraDeAreas()
+
+    def test_calcular_area_circulo(self):
+        self.assertAlmostEqual(self.calc.calcularAreaCirculo(1), math.pi)
+        self.assertAlmostEqual(self.calc.calcularAreaCirculo(0), 0)
+        self.assertRaises(ValueError, self.calc.calcularAreaCirculo, -1)
+
+    def test_calcular_area_quadrado(self):
+        self.assertEqual(self.calc.calcularAreaQuadrado(2), 4)
+        self.assertEqual(self.calc.calcularAreaQuadrado(0), 0)
+        self.assertRaises(ValueError, self.calc.calcularAreaQuadrado, -2)
+
+    def test_calcular_area_retangulo(self):
+        self.assertEqual(self.calc.calcularAreaRetangulo(2, 3), 6)
+        self.assertEqual(self.calc.calcularAreaRetangulo(0, 3), 0)
+        self.assertRaises(ValueError, self.calc.calcularAreaRetangulo, -2, 3)
+        self.assertRaises(ValueError, self.calc.calcularAreaRetangulo, 2, -3)
+        self.assertRaises(ValueError, self.calc.calcularAreaRetangulo, -2, -3)
+
+if __name__ == '__main__':
+    unittest.main()
